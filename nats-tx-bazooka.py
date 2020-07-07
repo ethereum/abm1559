0a8fd976a919fd6226eaa3463a01412be0fb3f50
barnabemonnot <b@barnabemonnot.com>
2020-07-07 11:48:30 +0200

    Fix json in nats txs

--- nats-tx-bazooka.py
+++ nats-tx-bazooka.py
@@ -1,33 +1,26 @@
+# https://github.com/nats-io/asyncio-nats-examples/blob/master/publish_json.py
+
 import asyncio
-from nats.aio.client import Client as NATS
-from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
-
 import json
+from nats.aio.client import Client as NATS
 
-async def run(loop):
+tx = {
+    "nonce": 1,
+    "value": 1000000000000000000,
+    "gasPremium": 1000000000,
+    "feeCap": 10000000000,
+}
+
+async def run():
     nc = NATS()
 
-    await nc.connect("demo.nats.io:4222", loop=loop)
+    await nc.connect(servers = ["demo.nats.io:4222"])
 
-    async def message_handler(msg):
-        subject = msg.subject
-        reply = msg.reply
-        data = msg.data.decode()
-        print("Received a message on '{subject} {reply}': {data}".format(
-            subject=subject, reply=reply, data=data))
-
-    # Simple publisher and async subscriber via coroutine.
-    sid = await nc.subscribe("foo", cb=message_handler)
-
-    await nc.publish("foo", json.loads('{"nonce":1,"value":1000000000000000000,"gasPremium":1000000000,"feeCap":10000000000}'))
-
-    # Remove interest in subscription.
-    await nc.unsubscribe(sid)
+    await nc.publish("txs", json.dumps(tx).encode())
 
     # Terminate connection to NATS.
     await nc.close()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop))
-    loop.close()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+loop.close()
